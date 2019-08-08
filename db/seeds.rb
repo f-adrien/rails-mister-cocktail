95f94447334b51275a43e4b9f7493a0b29c78ecc
Francois Adrien <francois.s.adrien@gmail.com>
2019-08-08 14:06:01 +0200

    seeds done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+require 'json'
+require 'open-uri'
+
+puts 'Cleaning database...'
+Ingredient.destroy_all
+
+puts 'Creating ingredients...'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_serialized = open(url).read
+
+ingredients = JSON.parse(ingredients_serialized)["drinks"]
+
+ingredients.each do |ingredient|
+  Ingredient.create({ name: ingredient["strIngredient1"] })
+end
+
+puts 'Finished!'
